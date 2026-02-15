b3d756d72aa842d7e829865457790c1a41b61492
antoniosxs <35566026+antoniosxs@users.noreply.github.com>
2026-02-15 12:06:40 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = 'kss-coach-v1.1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './kss_coach_192.png',
+  './kss_coach_512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(ASSETS))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
